65c197f685c4b36f4ebe6050f1bd1fcca2789151
darth-tesla <darth_tesla@protonmail.ch>
2019-12-01 09:23:39 +0300

    nav-bar & hamburger menu is done

--- js/main.js
+++ js/main.js
@@ -4,9 +4,39 @@
   const hamburgerBtn = document.querySelector(".hamburger");
 
   hamburgerBtn.addEventListener("click", isActive);
+  document.body.addEventListener("click", closeMenuByClickOnFog);
 
   function isActive() {
-    return hamburgerBtn.classList.toggle("is-active");
+    hamburgerBtn.classList.toggle("is-active");
+    toggleHamburgerMenu();
+    screenFog();
+    return;
+  }
+
+  function toggleHamburgerMenu() {
+    const hamburgerMenu = document.querySelector(".hamburger-menu__list");
+    hamburgerMenu.classList.toggle("hamburger-menu__list_is-visible");
+    return;
+  }
+
+  function screenFog() {
+    document.querySelector("div").classList.toggle("fog");
+    return;
+  }
+
+  function closeMenuByClickOnFog(el) {
+    const target = el.target;
+
+    if (
+      target.classList.contains("fog") ||
+      target.classList.contains("hamburger-menu__link")
+    ) {
+      hamburgerBtn.classList.remove("is-active");
+      toggleHamburgerMenu();
+      screenFog();
+      return;
+    }
+    return;
   }
 })();
 
