48cd3764bb1529473ac5a2a5f2ca2a2ae91ce500
darth-tesla <darth_tesla@protonmail.ch>
2019-12-04 19:13:22 +0300

    pluggin on autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const cleanCSS = require("gulp-clean-css");
+const autoprefixer = require("gulp-autoprefixer");
 const watch = require("gulp-watch");
 
 gulp.task("sass-compile", function() {
@@ -9,6 +10,11 @@ gulp.task("sass-compile", function() {
     .src("./scss/**/*.scss")
     .pipe(sourcemaps.init())
     .pipe(sass().on("error", sass.logError))
+    .pipe(
+      autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {
+        cascade: true
+      })
+    )
     .pipe(cleanCSS())
     .pipe(sourcemaps.write("./"))
     .pipe(gulp.dest("./css/"));
