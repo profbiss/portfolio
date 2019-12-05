6680d667b48fec7de166d22172fcc56be866a54d
darth-tesla <darth_tesla@protonmail.ch>
2019-12-05 08:31:29 +0300

    make some corrects | mailer not work

--- js/main.js
+++ js/main.js
@@ -49,3 +49,27 @@
 })();
 
 /* ********** hamburger menu ********** */
+
+/* ********** mailer menu ********** */
+
+$(document).ready(function() {
+  //E-mail Ajax Send
+  $(".form").submit(function() {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "../mail.php", //Change
+      data: th.serialize()
+    }).done(function() {
+      alert("Thank you!");
+      setTimeout(function() {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+});
+
+/* ********** mailer menu ********** */
