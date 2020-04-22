6ed070bf3808aa9f3440119ee03ab39c5a074485
Alexey <darth.tesla47@gmail.com>
2020-04-22 14:32:28 +0300

    rewrite all scripts on jquery

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
-$(document).ready(function () {
-  setTimeout(function () {
+$(document).ready(() => {
+  setTimeout(() => {
     if ($(".preloader").hasClass("done") === false)
       return $(".preloader").addClass("done");
   }, 1000);
 
   AOS.init({ once: !0 });
 
+  $(".hamburger").on("click", function () {
+    return $(this).toggleClass("is-active"), toggleMenu();
+  });
+  $(document).on("click", function (e) {
+    closeModal(e);
+    closeMenu(e);
+  });
+  $(window).on("scroll", () => {
+    if ($(this).scrollTop() <= 400) {
+      $(".to-top").removeAttr("style");
+    } else {
+      $(".to-top").attr("style", "display: flex;");
+    }
+  });
+  $(".to-top").on("click", (e) => {
+    e.preventDefault(), $("html").animate({ scrollTop: 0 }, 1e3);
+  });
   $(".form").submit(function () {
     var o = $(this);
     return (
@@ -20,54 +37,35 @@ $(document).ready(function () {
       !1
     );
   });
-});
 
-(function () {
-  const o = document.querySelector(".hamburger");
-  function e() {
-    document
-      .querySelector(".hamburger-menu__list")
-      .classList.toggle("hamburger-menu__list_is-visible");
+  function toggleMenu() {
+    $(".hamburger-menu__list").toggleClass("hamburger-menu__list_is-visible");
+    $(".fog").toggleClass("menu-fog");
   }
-  function t() {
-    document.querySelector(".fog").classList.toggle("menu-fog");
+  function closeMenu(e) {
+    const target = e.target;
+    if (
+      $(target).hasClass("menu-fog") ||
+      $(target).hasClass("hamburger-menu__link") ||
+      ($(target).hasClass("nav-bar__logo") && $(".fog").hasClass("menu-fog"))
+    ) {
+      $(".hamburger").removeClass("is-active"), toggleMenu();
+    }
   }
-  o.addEventListener("click", function () {
-    return o.classList.toggle("is-active"), e(), void t();
-  }),
-    document.body.addEventListener("click", function (s) {
-      const n = s.target;
-      return n.classList.contains("menu-fog") ||
-        n.classList.contains("hamburger-menu__link") ||
-        (n.classList.contains("nav-bar__logo") &&
-          document.querySelector("div").classList.contains("menu-fog"))
-        ? (o.classList.remove("is-active"), e(), void t())
-        : void 0;
-    });
-})();
-const modal = document.querySelector(".modal"),
-  modalFog = document.querySelector(".fog");
-function showModal() {
-  modal.classList.toggle("modal_visible"),
-    modalFog.classList.toggle("modal-fog");
-}
-function closeModal(o) {
-  const e = o.target;
-  (e.classList.contains("modal_visible") ||
-    e.classList.contains("modal-fog")) &&
-    (modal.classList.remove("modal_visible"),
-    modalFog.classList.remove("modal-fog"));
-}
-document.addEventListener("click", closeModal),
-  (function () {
-    $(window).on("scroll", () => {
-      if ($(this).scrollTop() <= 400) {
-        $(".to-top").removeAttr("style");
-      } else {
-        $(".to-top").attr("style", "display: flex;");
-      }
-    }),
-      $(".to-top").on("click", (e) => {
-        e.preventDefault(), $("html").animate({ scrollTop: 0 }, 1e3);
-      });
-  })();
+  function showModal() {
+    $(".modal").toggleClass("modal_visible");
+    $(".fog").toggleClass("modal-fog");
+  }
+  function closeModal(e) {
+    const target = e.target;
+    if (
+      $(target).hasClass("modal_visible") ||
+      $(target).hasClass("modal-fog")
+    ) {
+      $(".modal").removeClass("modal_visible"),
+        $(".fog").removeClass("modal-fog");
+    }
+  }
+
+  $(".form__btn").on("click", showModal);
+});
