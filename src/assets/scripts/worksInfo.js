2b39ed5e46f168529d14031fbed38ecd8049e0ff
Alexey Shevchenko <darth.tesla47@gmail.com>
2020-07-05 22:25:54 +0300

    add new functionality for cards

--- src/assets/scripts/worksInfo.js
+++ src/assets/scripts/worksInfo.js
@@ -1,10 +1,23 @@
-const workBtn = document.querySelector(".work__button");
-const workFog = document.querySelector(".work__fog");
+const workBtns = document.querySelectorAll(".work__button");
+const workFogs = document.querySelectorAll(".work__fog");
 const isVisibleFog = "work__fog_is-visible";
 
-workBtn.addEventListener("click", toggleInfo, true);
+workBtns.forEach((btn, index) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleInfo(index);
+  });
+});
 
-function toggleInfo(e) {
-  e.preventDefault();
-  workFog.classList.toggle(isVisibleFog);
+function toggleInfo(index) {
+  workBtns.forEach((btn, index2) => {
+    index === index2
+      ? btn.classList.toggle("work__button_is-active")
+      : btn.classList.remove("work__button_is-active");
+  });
+  workFogs.forEach((work, index2) => {
+    index === index2
+      ? work.classList.toggle(isVisibleFog)
+      : work.classList.remove(isVisibleFog);
+  });
 }
